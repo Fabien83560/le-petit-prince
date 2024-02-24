6cbfbfb3be562bc82d5241b7941c93d1040f0c87
Fabien83560 <fabienortega.290604@gmail.com>
2024-02-24 15:48:07 +0100

    Add User Interface Connexion

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonModal, IonItem, IonLabel, IonInput, IonCheckbox,
+         IonButton, IonIcon, IonText } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { person, personSharp, personOutline, lockClosed, lockClosedSharp, lockClosedOutline } from 'ionicons/icons';
+
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [FormsModule,ReactiveFormsModule, IonHeader, IonToolbar, IonTitle, IonContent, IonModal, IonItem, IonLabel, IonInput, IonCheckbox,
+            IonButton, IonIcon, IonText],
 })
 export class Tab3Page {
-  constructor() {}
+  userConnected: boolean = false
+  loginData = {
+    username: '',
+    password: ''
+  };
+
+  constructor() {
+    addIcons({ person, personSharp, personOutline, lockClosed, lockClosedSharp, lockClosedOutline })
+  }
+
+  async tryConnexion() {
+    this.userConnected = true;
+  }
 }
+
