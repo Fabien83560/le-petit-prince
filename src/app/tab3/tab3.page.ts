7c71ce7013ff36663ed7be7d43f8254830084afd
Fabien83560 <fabienortega.290604@gmail.com>
2024-02-24 16:56:34 +0100

    Ability to connect to a user account

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { NgIf } from '@angular/common';
 import { Preferences } from '@capacitor/preferences';
 import { IonHeader, IonToolbar, IonTitle, IonContent, IonModal, IonItem, IonLabel, IonInput, IonCheckbox,
          IonButton, IonIcon, IonText } from '@ionic/angular/standalone';
@@ -11,13 +12,17 @@ import { person, personSharp, personOutline, lockClosed, lockClosedSharp, lockCl
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, IonHeader, IonToolbar, IonTitle, IonContent, IonModal, IonItem, IonLabel, IonInput, IonCheckbox,
+  imports: [FormsModule, ReactiveFormsModule, NgIf, IonHeader, IonToolbar, IonTitle, IonContent, IonModal, IonItem, IonLabel, IonInput, IonCheckbox,
             IonButton, IonIcon, IonText],
 })
 
 export class Tab3Page implements OnInit {
   userConnected: boolean = false;
+
+  loginError: boolean = false;
+  loginErrorMessage: string = '';
   rememberLogin: boolean = false;
+
   loginData = {
     username: '',
     password: ''
@@ -41,26 +46,47 @@ export class Tab3Page implements OnInit {
   }
 
   async tryConnexion() {
-    this.userConnected = true;
-    if(this.rememberLogin) {
-      await Preferences.set({
-        key: 'rememberUsername',
-        value: JSON.stringify(this.loginData.username),
-      });
-      await Preferences.set({
-        key: 'rememberPassword',
-        value: JSON.stringify(this.loginData.password),
-      });
-    }
-    else {
-      await Preferences.set({
-        key: 'rememberUsername',
-        value: JSON.stringify(""),
-      });
-      await Preferences.set({
-        key: 'rememberPassword',
-        value: JSON.stringify(""),
-      });
+    const url = `http://www.sebastien-thon.fr/prince/index.php?connexion&login=${encodeURIComponent(this.loginData.username)}&mdp=${encodeURIComponent(this.loginData.password)}`;
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('Réponse réseau non réussie');
+      }
+
+      const data = await response.json();
+      if (data.resultat === "OK") {
+        this.userConnected = true;
+        this.loginError = false;
+        this.loginErrorMessage = '';
+
+        if(this.rememberLogin) {
+          await Preferences.set({
+            key: 'rememberUsername',
+            value: JSON.stringify(this.loginData.username),
+          });
+          await Preferences.set({
+            key: 'rememberPassword',
+            value: JSON.stringify(this.loginData.password),
+          });
+        }
+        else {
+          await Preferences.set({
+            key: 'rememberUsername',
+            value: JSON.stringify(""),
+          });
+          await Preferences.set({
+            key: 'rememberPassword',
+            value: JSON.stringify(""),
+          });
+        }
+      } else if (data.erreur) {
+        this.loginError = true;
+        this.loginErrorMessage = "Votre identifiant ou mot de passe est incorrect";
+      }
+    } catch (error) {
+      this.loginError = true;
+      this.loginErrorMessage = "Une erreur est survenue lors de la connexion";
     }
   }
 
@@ -71,4 +97,8 @@ export class Tab3Page implements OnInit {
       value: JSON.stringify(this.rememberLogin),
     });
   }
+
+  canDismissModal = () => {
+    return !this.userConnected;
+  };
 }
