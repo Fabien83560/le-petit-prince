fd2231980f9699b93eeda2bdadbfc8c7cd8b9e67
Fabien83560 <fabienortega.290604@gmail.com>
2024-03-14 11:19:29 +0100

    Add android folder

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'le-petit-prince',
+  appName: 'Le Petit Prince',
   webDir: 'www',
   server: {
     androidScheme: 'https'
